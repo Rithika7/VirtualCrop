de30494889f7689b499fdc83d2c14ea180e6cfe3
Rithika7 <70890335+Rithika7@users.noreply.github.com>
2020-11-24 12:09:06 +0530

    Add files via upload

---
+++ disp.js
@@ -0,0 +1,56 @@
+function st1()
+{
+    document.getElementById("st1").style.display="block";
+}
+function st2()
+{
+    document.getElementById("st2").style.display="block";
+}
+function st3()
+{
+    document.getElementById("st3").style.display="block";
+}
+function st4()
+{
+    document.getElementById("st4").style.display="block";
+}
+function st5()
+{
+    document.getElementById("st5").style.display="block";
+}
+function st6()
+{
+    document.getElementById("st6").style.display="block";
+}
+function st7()
+{
+    document.getElementById("st7").style.display="block";
+}
+function st8()
+{
+    document.getElementById("st8").style.display="block";
+}
+function st9()
+{
+    document.getElementById("st9").style.display="block";
+}
+function st10()
+{
+    document.getElementById("st10").style.display="block";
+}
+function st11()
+{
+    document.getElementById("st11").style.display="block";
+}
+function st12()
+{
+    document.getElementById("st12").style.display="block";
+}
+function st13()
+{
+    document.getElementById("st13").style.display="block";
+}
+function st14()
+{
+    document.getElementById("st14").style.display="block";
+}
